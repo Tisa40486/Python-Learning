19aa17ceb89539cfa3e26a143bea6391fcdcc657
Mattis <lefrancadammattis@gmail.com>
2026-06-15 16:38:10 +0200

    clean exercice

--- Exercice/VerifAge.py
+++ Exercice/VerifAge.py
@@ -2,16 +2,13 @@ print("------------------")
 print(" AGE VERIFICATION ")
 print("------------------")
 
-majority = 0 
 
 usaOrEurope = input("Choose rules : \n 1) usa \n 2) Europa \n")
-
 usaOrEurope = int(usaOrEurope)
-
-
 
 if usaOrEurope == 1:
     majority = 21
+    
 elif usaOrEurope == 2:
     majority = 18
     
@@ -19,11 +16,11 @@ else :
     print("Wrong input, read carefully.")
     quit()
     
-    
 age = input("Enter your age ")
 age = int(age)
 
 if  age >= majority :
     print("Adult")
 else : 
+    
     print("minor")
