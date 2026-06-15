138c01c097ab00cb3df0c6708f2041cdfc349719
Mattis <lefrancadammattis@gmail.com>
2026-06-15 10:37:04 +0200

    create fondamentals and exercice1

---
+++ Lab/Fondamentals.py
@@ -0,0 +1,17 @@
+# print("Hello World!")
+print("I am learning Python.", end=" ")
+print("It is awesome!")
+age = 35
+
+age = str(age)
+print("I am", age, "years old.")
+
+print("the type of age is:", type(age))
+
+x = "World"
+
+print("Hello " + x + "!")
+
+y = input("What is your name? ")
+
+print("Hello " + y + "!")
