814bd2e2866d459c12e7c19990150cdbe31f0c3a
Mattis <lefrancadammattis@gmail.com>
2026-06-25 16:00:19 +0200

    end of project final

--- SecondPhase/FinalProject/ToDoListWSave.py
+++ SecondPhase/FinalProject/ToDoListWSave.py
@@ -1,7 +1,11 @@
 import json
 import os
+from datetime import datetime
+
 folderName = "SecondPhase/FinalProject/Data"
 jsonFile = f"{folderName}/task.json"
+Task = []
+command = 0 
 Choice = {
     "Add" : 1,
     "List" : 2,
@@ -9,14 +13,17 @@ Choice = {
     "Quit" : 4
 }
 
+def writeLog(message : str) :
+    dt = datetime.now()
+    
+    with open(f"{folderName}/log.txt", "a") as file:
+        file.write(f"New log entry : {dt} \n")
+        file.write(f" --{message} \n")
 
 if not os.path.exists(jsonFile) or os.path.getsize(jsonFile) == 0:
     with open(jsonFile, "w") as f:
         json.dump([], f)
         
-Task = []
-command = 0 
-
 with open(f"{folderName}/task.json", "r") as file:
     loaded = json.load(file)
     Task = loaded
@@ -35,14 +42,22 @@ while command != Choice["Quit"]:
         taskName = input("Enter task name : ")
         Task.append(taskName)
         print("Added!")
+        writeLog(f" \"{taskName}\" added Successfully")
         
     if command == Choice["List"]:
         print(Task)
+        writeLog(f"The list was printed")
         
     if command == Choice["Remove"]:
-        element = input("Choose a Task to remove ")
-        element = str(element)
-        Task.remove(element)
+        try:
+            element = input("Choose a Task to remove ")
+            element = str(element)
+            Task.remove(element)
+            writeLog(f" \"{element}\" was removed.")
+        except Exception as e:
+            print("Oups problems, retry :(")
+            writeLog(f" \"{e}\" run into a problem")
+            
         
 with open(f"{jsonFile}", "w") as file:
         json.dump(Task,file)
