b5ec109009c810bc1805ba92712d2edda03f902a
Mattis <lefrancadammattis@gmail.com>
2026-07-16 11:47:13 +0200

    fastApi first stuff

---
+++ ThirdPhase/FastApi/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return{"message":"Hello World"}
+
+
+@app.get("/users/{user_id}")
+def read_user(user_id: int):
+    return{"user_id": user_id, "name": "Username"}
